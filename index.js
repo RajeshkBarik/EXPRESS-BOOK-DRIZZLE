f0a3db3da495dd7ab6b9d70e0119e336479d6e33
RajeshkBarik <122564156+RajeshkBarik@users.noreply.github.com>
2026-04-08 22:49:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv/config');
+const express = require('express');
+
+const { loggerMiddleware } = require('./middelwares/logger');
+
+const bookRouter = require('./routes/book.routes');
+
+const app = express();
+const PORT = 8000;
+
+// Middlewares (Plugins)
+app.use(express.json());
+app.use(loggerMiddleware);
+
+// Routes
+app.use('/books', bookRouter);
+
+app.listen(PORT, () => console.log(`Http server is running on PORT ${PORT}`));
